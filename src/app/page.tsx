db3ec4b8e4e96b010b2e7a0c7388c7b19d0a3b54
riyuc <bongbang0101@gmail.com>
2024-01-23 16:19:25 -0500

    chore: add separator

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,21 +2,28 @@
 
 import Image from "next/image";
 import Header from "../components/header";
+import Separator from "../components/separator";
 
 export default function Home() {
   return (
     <div className="whitespace-nowrap overflow-auto no-scrollbar">
       <div className="flex h-screen flex-col items-center">
-        <div className="flex flex-row justify-center gap-2 items-center w-full h-screen">
-          <h1 className="font-jp">
-          私の名前は
+        <div className="flex flex-row justify-center gap-3 text-4xl items-center w-full h-screen">
+          <h1 className="font-sans font-extralight">
+          Hi, My name is
           </h1>
-          <button>
-            Hello
+          <button className="font-sans font-extralight text-black-500 text-4xl">
+            Duc Anh Nguyen
           </button>
         </div>
-        <div>
-          cdf
+        <Separator />
+        <div className="flex flex-row justify-center gap-2 text-4xl items-center w-full h-screen">
+          <h1 className="font-jp">
+          初めまして、
+          </h1>
+          <button className="text-purple-300 text-4xl font-medium">
+            りゆちです。
+          </button>
         </div>
       </div>
     </div>
